0ad1a5cfb7842360b906d0e35658b34bc03e4ca5
Vishwas Gowda <vishwasgowda@Vishwass-MacBook-Air.local>
2023-05-10 21:00:15 +0530

    mlflow experiment tracking

--- src/train.py
+++ src/train.py
@@ -1,4 +1,6 @@
 from typing import List
+import mlflow
+import mlflow.sklearn
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -22,6 +24,7 @@ dt = DecisionTreeClassifier()
 rf = RandomForestClassifier()
 
 models = [lr, svc, dt, rf]
+model_name = ["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
 scores = []
 
 
@@ -31,7 +34,8 @@ def model1(df: pd.DataFrame):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    for m in models:
+
+    for i, m in enumerate(models):
         m.fit(X_train, y_train)
         y_pred = m.predict(X_test)
         acc = accuracy_score(y_test, y_pred) * 100
@@ -39,6 +43,13 @@ def model1(df: pd.DataFrame):
         rec = recall_score(y_test, y_pred) * 100
         f1 = f1_score(y_test, y_pred) * 100
         scores.append([acc, prec, rec, f1])
+
+        with mlflow.start_run():
+            mlflow.sklearn.log_model(m, model_name[i])
+            mlflow.log_metric("Accuracy", acc)
+            mlflow.log_metric("Precision", prec)
+            mlflow.log_metric("Recall", rec)
+            mlflow.log_metric("F1", f1)
 
     scores_df = pd.DataFrame(
         columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
@@ -49,11 +60,19 @@ def model1(df: pd.DataFrame):
     best_model_idx = scores_df["F1"].idxmax()
     best_model = models[best_model_idx]
 
+    # mlflow.sklearn.log_model(best_model, "best_model")
+
     best_model.fit(X_train, y_train)
     y_pred = best_model.predict(X_test)
     report = classification_report(y_test, y_pred)
+
+    # mlflow.log_text('Classification Report', report)
+
     logger.info("Best Model: {}".format(best_model))
     logger.info(f"Classification Report:\n{report}")
+
+
+
     return scores_df, best_model, report
 
 
@@ -63,7 +82,7 @@ def model2(df: pd.DataFrame):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    for m in models:
+    for i, m in enumerate(models):
         m.fit(X_train, y_train)
         y_pred = m.predict(X_test)
         acc = accuracy_score(y_test, y_pred) * 100
@@ -71,6 +90,13 @@ def model2(df: pd.DataFrame):
         rec = recall_score(y_test, y_pred, average="macro") * 100
         f1 = f1_score(y_test, y_pred, average="macro") * 100
         scores.append([acc, prec, rec, f1])
+
+        with mlflow.start_run():
+            mlflow.sklearn.log_model(m, model_name[i])
+            mlflow.log_metric("Accuracy", acc)
+            mlflow.log_metric("Precision", prec)
+            mlflow.log_metric("Recall", rec)
+            mlflow.log_metric("F1", f1)
 
     scores_df = pd.DataFrame(
         columns=["Model"], data=["Logistic Regression", "SVC", "Decision Tree", "Random Forest"]
